72f09b71a2598d860a70a2d23383b000b496ee59
Makcim2011 <96636479+Makcim2011@users.noreply.github.com>
2022-09-16 09:15:48 +0300

    add update for bunner BunnerHeader

--- src/components/BunnerHeader.vue
+++ src/components/BunnerHeader.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="navbar navbar-dark bg-primary">
+  <nav class="navbar fixed-top navbar-dark bg-primary">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="navbar-brand mb-0 h1" to="/">Home</router-link> |
+      <router-link to="/productCart">Cart (0)</router-link>
     </div>
   </nav>
 </template>
---
+++ src/views/ProductCart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <span class="navbar-brand mb-0">Cart (0)</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProductCart',
+    components: {}
+  }
+  </script>
+  
+  <style>
+  
+  </style>
